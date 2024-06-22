4263a4bb326aceeaf542aa46f0f82fe3b01fe331
Wilda555 <wildasatria555@gmail.com>
2024-06-22 12:09:18 +0700

    first commit

---
+++ src/test/java/runners/testRunnerAPI.java
@@ -0,0 +1,16 @@
+package runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"pretty", "html:target/cucumber-report.html"},
+        glue = {"stepDef"},
+        features = {"src/test/java/features"},
+        tags = "@api",
+        monochrome = true
+)
+public class testRunnerAPI {
+}
